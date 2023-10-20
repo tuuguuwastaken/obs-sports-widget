287d7ab7c700ec78a18138fa4467e07ac63ab9d1
tuguldurjon <tuguldur.j@ailab.mn>
2023-10-20 11:16:01 +0800

    init

--- src/components/sportsCounter.js
+++ src/components/sportsCounter.js
@@ -4,10 +4,32 @@ import img from "../assets/istockphoto-1471979698-612x612(2).png";
 import { Col, Row } from "antd";
 
 const SportsCounter = () => {
+  const storedTeamTwoPoints = localStorage.getItem("teamTwo");
+  const initialTeamTwoPoints = storedTeamTwoPoints
+    ? parseInt(storedTeamTwoPoints)
+    : 0;
+
+  const storedTeamOnePoints = localStorage.getItem("teamTwo");
+  const initialTeamOnePoints = storedTeamOnePoints
+    ? parseInt(storedTeamTwoPoints)
+    : 0;
+  const [teamOnePoints, setTeamOnePoints] = useState(initialTeamOnePoints);
+
+  const [teamTwoPoints, setTeamTwoPoints] = useState(initialTeamTwoPoints);
   const [teamOneName, setTeamOneName] = useState("alr");
   const [teamTwoName, setTeamTwoName] = useState("nay");
-  const [teamOnePoints, setTeamOnePoints] = useState(122);
-  const [teamTwoPoints, setTeamTwoPoints] = useState(222);
+
+  const addTeamOne = () => {
+    let i = teamOnePoints;
+    setTeamOnePoints(i + 1);
+    localStorage.setItem("teamOne", teamOnePoints);
+  };
+  const addTeamTwo = () => {
+    let i = teamTwoPoints;
+
+    setTeamTwoPoints(i + 1);
+    localStorage.setItem("teamTwo", teamTwoPoints);
+  };
 
   return (
     <div className="sports-container">
@@ -15,7 +37,7 @@ const SportsCounter = () => {
       <Row>
         <Col span={2}>
           <Row justify={"center"}>
-            <h1>{teamOneName}</h1>
+            <h1 onClick={addTeamOne}>{teamOneName}</h1>
           </Row>
         </Col>
         <Col span={2}>
@@ -25,7 +47,7 @@ const SportsCounter = () => {
         </Col>
         <Col span={2}>
           <Row justify={"center"}>
-            <h1>{teamTwoName}</h1>
+            <h1 onClick={addTeamTwo}>{teamTwoName}</h1>
           </Row>
         </Col>
       </Row>
